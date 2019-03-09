b851d3f88e7c25069ac131297c8ad81988c8eb72
felcilive <47640299+felcilive@users.noreply.github.com>
2019-03-09 18:42:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var maxi, deli;
+    var min = 0;
+    var n = number;
+    var sum1 = 0, sum2 = 0;
+    var simple = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251];
+    var divideSimple = {};
+
+
+    for (var i = 0, j = 0; base > 1; i++) {
+        while (base % simple[i] == 0) {
+            if (simple[i] in divideSimple)
+                divideSimple[simple[i]] += 1;
+            else
+                divideSimple[simple[i]] = 1;
+            base = base / simple[i];
+
+        }
+
+    }
+    var divideSimpleKeys = (Object.keys(divideSimple));
+    var max = 0;
+
+    for (var i = 0; i < divideSimpleKeys.length; i++) {
+        a = (divideSimpleKeys[i]);
+        var b = (divideSimple[divideSimpleKeys[i]]);
+        var pow = (Math.pow(a, b));
+        if (pow > max) {
+            max = pow;
+            maxi = +a;
+            deli = +b;
+        }
+        if (+a > +min)
+            min = a;
+
+
+    }
+
+    while (number !=0) {
+        number = Math.floor(number / maxi);
+        sum1 += number;
+
+
+        n = Math.floor(n / min);
+        sum2 += n;
+    }
+    sum1 = Math.floor(sum1 / deli);
+    console.log('sum1=' + sum1);
+    console.log('sum2=' + sum2);
+    if (sum1 < sum2)
+        return sum1;
+    else return sum2;
 }
+
